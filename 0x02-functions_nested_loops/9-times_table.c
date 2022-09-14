584d3453c356c4152864d8450efa0655d06df630
Adebukola05 <ayoadebukola2017@gmail.com>
2022-09-13 17:39:03 -0700

    c commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * time_table - that prints the 9 times table
+ * times_table - that prints the 9 times table
  *
  * Return: no return
  */
@@ -13,19 +13,19 @@ void times_table(void)
 		_putchar(48);
 		for (b = 1; b <= 9; b++)
 		{
-			c = a * b;
-			_putchar(44);
+		c = a * b;
+		_putchar(44);
+		_putchar(32);
+		if (c <= 9)
+		{
 			_putchar(32);
-			if (c <= 9)
-			{
-				_putchar(32);
-				_putchar(c + 48);
-			}
-			else
-			{
-				_putchar((c / 10) + 48);
-				_putchar((c % 10) + 48);
-			}
+			_putchar(c + 48);
+		}
+		else
+		{
+			_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
+		}
 		}
 		_putchar('\n');
 	}
